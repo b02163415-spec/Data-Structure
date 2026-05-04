d3259b38ed083f679c7bad371c4fc6820cfad17f
b02163415-spec <b02163415@gmail.com>
2026-05-04 13:34:46 +0530

    Add files via upload

---
+++ REFERENCE UPDATE.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void update(struct student*);
+struct student
+{
+	int rno;
+	int marks;
+};
+int main()
+{
+	struct student s1;
+	s1.rno=1;
+	s1.marks=450;
+	update(&s1);
+	printf("%d",s1.marks);
+	return 0;
+}
+void update(struct student *s1)
+{
+	s1->marks=s1->marks+5;
+}
