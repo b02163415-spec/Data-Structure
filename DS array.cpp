65f3cd4ab9cdf135c5cd45d01820013c04b1ca00
b02163415-spec <b02163415@gmail.com>
2026-05-04 13:28:08 +0530

    Add files via upload

---
+++ DS array.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct student 
+{
+	int rno;
+	char name[70];
+};
+int main()
+{
+	struct student s[20];
+	int i;
+	for(i=0;i<3;i++)
+	{
+		printf("enter %d student data\n",i);
+		scanf("%d%s",&s[i].rno,s[i].name);
+	}
+	printf("student details\n");
+    for(i=0;i<3;i++)
+	{
+		printf("%d %s\n",s[i].rno,s[i].name);
+	}
+	return 0;
+}
